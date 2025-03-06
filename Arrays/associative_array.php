791b627188e263f65369808e3f0eaa3d05eb83f8
Apurv66 <159132106+Apurv66@users.noreply.github.com>
2025-03-06 10:25:12 +0530

    Add files via upload

---
+++ Arrays/associative_array.php
@@ -0,0 +1,10 @@
+<?php
+    $arr = array(
+        1=>"monday",
+        2=>"tusday",
+        3=>"wedesday"
+    );
+    foreach($arr as $key=>$value){
+        echo $key." => ".$value . "<br>";
+    }
+?>
