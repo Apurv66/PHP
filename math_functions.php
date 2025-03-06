4feb855a2fd77f215447a6d47ea7dca073308377
Apurv66 <159132106+Apurv66@users.noreply.github.com>
2025-03-06 11:17:41 +0530

    Add files via upload

---
+++ math_functions.php
@@ -0,0 +1,29 @@
+<?php
+    echo sqrt(49);
+    echo "<br>";
+    
+    echo ceil(49.33);
+    echo "<br>";
+    
+    echo floor(49.33);
+    echo "<br>";
+    
+    echo round(33.51);
+    echo "<br>";
+    
+    echo min(3,4,5);
+    echo "<br>";
+    
+    echo max(3,4,5);
+    echo "<br>";
+    
+    echo pow(3,2);
+    echo "<br>";
+    
+    echo exp(4);
+    echo "<br>";
+    
+    echo rand(10,12);
+    echo "<br>";
+
+?>
