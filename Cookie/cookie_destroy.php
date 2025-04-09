749a69b3c1c91631e30908a635637f143a93bb61
Apurv66 <159132106+Apurv66@users.noreply.github.com>
2025-04-09 09:46:51 +0530

    Add files via upload

---
+++ Cookie/cookie_destroy.php
@@ -0,0 +1,4 @@
+<?php
+    setcookie("name", "abc", time()-3600);
+    echo "cookie is destroy...";
+?>
---
+++ Cookie/cookie_set.php
@@ -0,0 +1,4 @@
+<?php
+    setcookie("name", "abc", time()+78200);
+    echo "cookie is created...";
+?>
