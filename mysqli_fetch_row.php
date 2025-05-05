f716907740c093425567520e1d2eb8f2c0e51b17
Apurv66 <159132106+Apurv66@users.noreply.github.com>
2025-05-05 11:51:39 +0530

    Add files via upload

---
+++ mysqli_fetch_row.php
@@ -0,0 +1,19 @@
+<?php
+    $conn = mysqli_connect("localhost", "root", "");
+    mysqli_select_db($conn,"test");
+    $qry = "select * from cric";
+    $result = mysqli_query($conn,$qry);
+    while($row = mysqli_fetch_row($result)){
+        echo "id = " . $row[0];
+        echo "<br>";
+        echo "name = " . $row[1];
+        echo "<br>";
+    }
+
+    if(!$result){
+        echo "failed!!";
+    }
+    else{
+        echo "success...";
+    }
+?>
